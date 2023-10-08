b583d52d27778ed5db97ee44169935b9c6fe0322
Pat <patrick@guerilla360.com>
2023-10-08 18:58:30 +0800

    pat: rename images

--- src/app/components/photos.js
+++ src/app/components/photos.js
@@ -1,19 +1,18 @@
 const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
 
-const unsplashLink = (id, width, height) =>
-    `https://source.unsplash.com/${id}/${width}x${height}`;
+const unsplashLink = (filename) => `images/${filename}`;
 
 const unsplashPhotos = [
-    { id: "-heLWtuAN3c", width: 1080, height: 608 },
-    { id: "xOigCUcFdA8", width: 1080, height: 720 },
-    { id: "X48pUOPKf7A", width: 1080, height: 160 },
-    { id: "1azAjl8FTnU", width: 1080, height: 1549 },
-    { id: "ALrCdq-ui_Q", width: 1080, height: 720 },
-    { id: "twukN12EN7c", width: 1080, height: 694 },
-    { id: "9UjEyzA6pP4", width: 1080, height: 1620 },
-    { id: "sEXGgun3ZiE", width: 1080, height: 720 },
-    { id: "S-cdwrx-YuQ", width: 1080, height: 1440 },
-    { id: "q-motCAvPBM", width: 1080, height: 1620 },
+    { filename: "1.jpg", width: 1080, height: 720 },
+    { filename: "2.JPG", width: 1080, height: 720 },
+    { filename: "3.jpg", width: 1080, height: 260 },
+    { filename: "4.jpg", width: 1080, height: 1549 },
+    { filename: "5.jpg", width: 1080, height: 720 },
+    { filename: "6.jpg", width: 1080, height: 720 },
+    { filename: "9UjEyzA6pP4", width: 1080, height: 1620 },
+    { filename: "sEXGgun3ZiE", width: 1080, height: 720 },
+    { filename: "S-cdwrx-YuQ", width: 1080, height: 1440 },
+    { filename: "q-motCAvPBM", width: 1080, height: 1620 },
 ];
 
 const photos = unsplashPhotos.map((photo) => {
@@ -21,13 +20,13 @@ const photos = unsplashPhotos.map((photo) => {
     const height = (photo.height / photo.width) * width;
 
     return {
-        src: unsplashLink(photo.id, width, height),
+        src: unsplashLink(photo.filename),
         width,
         height,
         srcSet: breakpoints.map((breakpoint) => {
             const height = Math.round((photo.height / photo.width) * breakpoint);
             return {
-                src: unsplashLink(photo.id, breakpoint, height),
+                src: unsplashLink(photo.filename),
                 width: breakpoint,
                 height,
             };
